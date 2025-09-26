b2545a58fd62256582cf33fe3631494746555fd0
GingerNinjaProgramming <connervian@outlook.com>
2025-09-26 22:21:50 +0100

    Grid Based Movement
    Used a constant distance per movement and constant width and height per object to represent a grid like structure for snake playing

--- main.cpp
+++ main.cpp
@@ -5,32 +5,38 @@ using namespace std;
 
 enum Direction { UP, DOWN, LEFT, RIGHT, NONE };
 
-class GameObject {
+class SnakeObject {
     public:
         int x;
         int y;
         int width;
         int height;
+        Direction direction;
 
-    GameObject(int x, int y, int width, int height) {
+    SnakeObject(int x, int y, int width, int height, Direction dir){
         this->x = x;
         this->y = y;
         this->width = width;
         this->height = height;
+        this->direction = dir;
     }
 };
 
 int main () {
-
     const int SCREEN_WIDTH = 800;
     const int SCREEN_HEIGHT = 600;
 
-    GameObject snakeHead(400, 300, 20, 20);
+    const int GRID_CELL_SIZE = 20;
+
+    int food_x = 600;
+    int food_y = 300;
+
+    SnakeObject snakeHead(0, 0, GRID_CELL_SIZE, GRID_CELL_SIZE, NONE);
 
     Direction direction = NONE;
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Snake");
-    SetTargetFPS(60);
+    SetTargetFPS(15);
 
     while (!WindowShouldClose()){
 
@@ -41,16 +47,16 @@ int main () {
 
         switch(direction){
             case RIGHT:
-                snakeHead.x += 5;
+                snakeHead.x += 1 * GRID_CELL_SIZE;
                 break;
             case LEFT:
-                snakeHead.x -= 5;
+                snakeHead.x -= 1 * GRID_CELL_SIZE;
                 break;
             case UP:
-                snakeHead.y -= 5;
+                snakeHead.y -= 1 * GRID_CELL_SIZE;
                 break;
             case DOWN:
-                snakeHead.y += 5;
+                snakeHead.y += 1 * GRID_CELL_SIZE;
                 break;
             case NONE:
                 // Do nothing
@@ -59,9 +65,12 @@ int main () {
                 break;
         }
 
+        
+
         BeginDrawing();
             ClearBackground(BLACK);
             DrawRectangle(snakeHead.x, snakeHead.y, snakeHead.width, snakeHead.height, GREEN);
+            DrawRectangle(food_x, food_y, GRID_CELL_SIZE, GRID_CELL_SIZE, RED);
         EndDrawing();
     }
 
