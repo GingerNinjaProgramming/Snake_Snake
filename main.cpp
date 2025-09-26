1eeb5282fb8172a6cc6c83618cac7815263d8d2a
GingerNinjaProgramming <connervian@outlook.com>
2025-09-26 21:17:47 +0100

    GameObject Class
    I have created a gameobject class to clean up logic for snake

--- main.cpp
+++ main.cpp
@@ -3,39 +3,36 @@
 
 using namespace std;
 
+class GameObject {
+    public:
+        int x;
+        int y;
+        int width;
+        int height;
+
+    GameObject(int x, int y, int width, int height) {
+        this->x = x;
+        this->y = y;
+        this->width = width;
+        this->height = height;
+    }
+};
+
 int main () {
 
     const int SCREEN_WIDTH = 800;
     const int SCREEN_HEIGHT = 600;
-    int ball_x = 100;
-    int ball_y = 100;
-    int ball_speed_x = 5;
-    int ball_speed_y = 5;
-    int ball_radius = 15;
 
-    cout << "Hello World" << endl;
+    GameObject snakeHead(400, 300, 20, 20);
 
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "My first RAYLIB program!");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Snake");
     SetTargetFPS(60);
 
     while (WindowShouldClose() == false){
-   
-        ball_x += ball_speed_x;
-        ball_y += ball_speed_y;
-
-        if(ball_x + ball_radius >= SCREEN_WIDTH || ball_x - ball_radius <= 0)
-        {
-            ball_speed_x *= -1;
-        }
-
-        if(ball_y + ball_radius >= SCREEN_HEIGHT || ball_y - ball_radius <= 0)
-        {
-            ball_speed_y *= -1;
-        }
         
         BeginDrawing();
             ClearBackground(BLACK);
-            DrawCircle(ball_x,ball_y,ball_radius, WHITE);
+            DrawRectangle(snakeHead.x, snakeHead.y, snakeHead.width, snakeHead.height, GREEN);
         EndDrawing();
     }
 
