0a29a4e1e2c304be62a13bfff49a97817bf113d7
GingerNinjaProgramming <connervian@outlook.com>
2025-09-28 22:52:46 +0100

    Cleaning, GameOver and Colorful Snake!
    Cleaned up some code by refactoring functionality into seperate and bringing most variables out of main() to allow for easyier use across functions
    
    Added a game over while basic it works
    
    Messed around with snake colour with a blue head and alternating green and dark green pattern on body
    
    	new file:   launch.exe
    	modified:   main.cpp
    	modified:   main.exe
    	new file:   main.h
    	modified:   main.cpp
    	modified:   main.exe
    	modified:   main.h

--- main.cpp
+++ main.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
 #include <raylib.h>
 #include <queue>
+#include "main.h"
 
 using namespace std;
 
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
 const int GRID_CELL_SIZE = 20;
-
-//The difficulty enum values represent the number of frames between each snake movement
-enum Difficulty { EASY = 10, MEDIUM = 5, HARD = 1};
-
-enum Direction { UP, DOWN, LEFT, RIGHT, NONE };
-enum GameState { START, PLAYING, GAMEOVER, PAUSED};
+const int MOVE_FRAME_DELAY = 10;
 
 class SnakeObject {
     public:
         int x;
         int y;
-        int width;
-        int height;
+        int width = GRID_CELL_SIZE;
+        int height = GRID_CELL_SIZE;
 
-    SnakeObject(int x, int y, int width, int height){
+    SnakeObject(int x, int y){
         this->x = x;
         this->y = y;
-        this->width = width;
-        this->height = height;
     }
 
     bool IsCollidingWith(int otherX, int otherY){
@@ -31,15 +28,47 @@ class SnakeObject {
     }
 };
 
-void CreateSnake(queue<SnakeObject*> snake){
-    while(!snake.empty()){
-        SnakeObject* segment = snake.front();
-        snake.pop();
-        DrawRectangle(segment->x, segment->y, segment->width, segment->height, GREEN);
+//The difficulty enum values represent the number of frames between each snake movement
+enum Difficulty { EASY = 10, MEDIUM = 5, HARD = 1};
+
+enum Direction { UP, DOWN, LEFT, RIGHT, NONE };
+enum GameState { START, PLAYING, GAMEOVER, PAUSED};
+
+queue<SnakeObject*> snake;
+
+Direction direction = NONE;
+GameState gameState = PLAYING;
+
+bool inputBuffing = true; 
+bool gameStarted = false; // To initialize the snake only once on first frame of PLAYING state
+bool shouldMove = false; 
+bool shouLdClose = false;
+
+int food_x = 600;
+int food_y = 300;
+
+int score = 0;
+int frameCounter = 0;
+
+SnakeObject GetSnakeHead(){
+    return *snake.front();
+}
+
+void CreateSnake(queue<SnakeObject*> _snake){
+    bool isHead = true;
+
+    while(!_snake.empty()){
+        SnakeObject* segment = _snake.front();
+        _snake.pop();
+
+        Color color = isHead ? BLUE : (_snake.size() % 2 == 0 ? DARKGREEN : GREEN);
+
+        DrawRectangle(segment->x, segment->y, segment->width, segment->height, color);
+        isHead = false;
     }
 } 
 
-void UpdateSnake(Direction direction,queue<SnakeObject*> &snake, bool shouldMove){
+void UpdateSnake(bool shouldMove){
     if(!shouldMove) return;
 
     queue<SnakeObject*> tempSnake;
@@ -87,11 +116,11 @@ void UpdateSnake(Direction direction,queue<SnakeObject*> &snake, bool shouldMove
     snake = tempSnake;
 }
 
-bool HandleDeathCollisions(queue<SnakeObject*> snake, int screenWidth, int screenHeight) {
+bool HandleDeathCollisions(queue<SnakeObject*> snake) {
     SnakeObject* head = snake.front();
     snake.pop();
 
-    if (head->x < 0 || head->x >= screenWidth || head->y < 0 || head->y >= screenHeight) {
+    if (head->x < 0 || head->x >= SCREEN_WIDTH || head->y < 0 || head->y >= SCREEN_HEIGHT) {
         return true; // Collision with wall
     }
 
@@ -106,92 +135,138 @@ bool HandleDeathCollisions(queue<SnakeObject*> snake, int screenWidth, int scree
     return false;
 }
 
+void HandlePlayingState() {
+    // Implement the logic for the PLAYING state here
+    if(snake.empty()){
+        SnakeObject head(0,0);
+        snake.push(&head);
+    }
+
+    int previousSnakeBackX = snake.back()->x;
+    int previousSnakeBackY = snake.back()->y;
+
+    shouldMove = frameCounter == MOVE_FRAME_DELAY;
+
+    if(shouldMove) frameCounter = 0;
+    else frameCounter++;
+
+    HandleMovement();
+
+    if(GetSnakeHead().IsCollidingWith(food_x, food_y)){
+        SnakeObject* newSegment = new SnakeObject(previousSnakeBackX, previousSnakeBackY);
+        snake.push(newSegment);
+
+        food_x = GetRandomValue(0, (SCREEN_WIDTH / GRID_CELL_SIZE) - 1) * GRID_CELL_SIZE;
+        food_y = GetRandomValue(0, (SCREEN_HEIGHT / GRID_CELL_SIZE) - 1) * GRID_CELL_SIZE;
+    }
+
+    if(HandleDeathCollisions(snake)){
+        cout << "Game Over!" << endl;
+        gameState = GAMEOVER;
+        return;
+    }
+
+    score = snake.size() - 1;
+
+    BeginDrawing();
+        ClearBackground(BLACK);
+        CreateSnake(snake);
+        DrawRectangle(food_x, food_y, GRID_CELL_SIZE, GRID_CELL_SIZE, RED);
+        DrawText(("Score: " + to_string(score)).c_str(), 10, 10, 20, WHITE);
+    EndDrawing();
+}
+
+void HandleMovement()
+{
+    if (inputBuffing)
+    {
+        queue<Direction> inputBuffer;
+
+        if ((IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) && direction != LEFT)
+            inputBuffer.push(RIGHT);
+        else if ((IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) && direction != RIGHT)
+            inputBuffer.push(LEFT);
+        else if ((IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) && direction != DOWN)
+            inputBuffer.push(UP);
+        else if ((IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) && direction != UP)
+            inputBuffer.push(DOWN);
+
+        if (!inputBuffer.empty())
+        {
+            direction = inputBuffer.front();
+            inputBuffer.pop();
+
+            UpdateSnake(shouldMove);
+        }
+        else
+        {
+            UpdateSnake(shouldMove);
+        }
+    }
+    else
+    {
+
+        if ((IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) && direction != LEFT)
+            direction = RIGHT;
+        else if ((IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) && direction != RIGHT)
+            direction = LEFT;
+        else if ((IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) && direction != DOWN)
+            direction = UP;
+        else if ((IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) && direction != UP)
+            direction = DOWN;
+
+        UpdateSnake(shouldMove);
+    }
+}
 int main () {
-    const int SCREEN_WIDTH = 800;
-    const int SCREEN_HEIGHT = 600;
-    const int MOVE_FRAME_DELAY = 10;
-
-    bool inputBuffing = true;
-    bool shouldMove = false;
-
-    int food_x = 600;
-    int food_y = 300;
-
-    int score = 0;
-    int frameCounter = 0;
-
-    queue<SnakeObject*> snake;
-
-    SnakeObject snakeHead(0, 0, GRID_CELL_SIZE, GRID_CELL_SIZE);
-    snake.push(&snakeHead);
-
-    Direction direction = NONE;
-
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Snake");
     SetTargetFPS(60);
 
-    while (!WindowShouldClose()){
-        int previousSnakeBackX = snake.back()->x;
-        int previousSnakeBackY = snake.back()->y;
+    gameStarted = true;
+    while (!shouLdClose){
+        switch(gameState){
+            case START:
+                break;
+            case PLAYING:
+                HandlePlayingState();
+                break;
+            case GAMEOVER:
+                if(IsKeyDown(KEY_R)){
+                    // Reset game state
+                    ResetValues();
+                }
 
-        shouldMove = frameCounter == MOVE_FRAME_DELAY;
-
-        if(shouldMove) frameCounter = 0;
-        else frameCounter++;
-
-        if(inputBuffing){
-            queue<Direction> inputBuffer;
-
-            if((IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) && direction != LEFT) inputBuffer.push(RIGHT);
-            else if((IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) && direction != RIGHT) inputBuffer.push(LEFT);
-            else if((IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) && direction != DOWN) inputBuffer.push(UP);
-            else if((IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) && direction != UP) inputBuffer.push(DOWN);
-
-            if(!inputBuffer.empty()){
-                direction = inputBuffer.front();
-                inputBuffer.pop();
-
-                UpdateSnake(direction,snake,shouldMove);
-            }else{
-                UpdateSnake(direction,snake,shouldMove);
-            }
-
-        }else{
-
-            if((IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) && direction != LEFT) direction = RIGHT;
-            else if((IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) && direction != RIGHT) direction = LEFT;
-            else if((IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) && direction != DOWN) direction = UP;
-            else if((IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) && direction != UP) direction = DOWN;
-
-            
-            UpdateSnake(direction,snake,shouldMove);
+                BeginDrawing();
+                    ClearBackground(BLACK);
+                    DrawText("Game Over! Press R to Restart or ESC to Exit", 100, SCREEN_HEIGHT / 2 - 20, 20, WHITE);
+                    DrawText(("Final Score: " + to_string(score)).c_str(), 100, SCREEN_HEIGHT / 2 + 20, 20, WHITE);
+                EndDrawing();
+                break;
+            case PAUSED:
+                break;
         }
 
-        if(snakeHead.IsCollidingWith(food_x, food_y)){
-            SnakeObject* newSegment = new SnakeObject(previousSnakeBackX, previousSnakeBackY, GRID_CELL_SIZE, GRID_CELL_SIZE);
-            snake.push(newSegment);
-
-            food_x = GetRandomValue(0, (SCREEN_WIDTH / GRID_CELL_SIZE) - 1) * GRID_CELL_SIZE;
-            food_y = GetRandomValue(0, (SCREEN_HEIGHT / GRID_CELL_SIZE) - 1) * GRID_CELL_SIZE;
+        if(WindowShouldClose() && !IsKeyPressed(KEY_ESCAPE)) {
+            shouLdClose = true;
+            continue;
         }
-
-        if(HandleDeathCollisions(snake, SCREEN_WIDTH, SCREEN_HEIGHT)){
-            cout << "Game Over!" << endl;
-            break;
-        }
-
-        score = snake.size() - 1;
-
-        cout << direction << endl;
-
-        BeginDrawing();
-            ClearBackground(BLACK);
-            CreateSnake(snake);
-            DrawRectangle(food_x, food_y, GRID_CELL_SIZE, GRID_CELL_SIZE, RED);
-            DrawText(("Score: " + to_string(score)).c_str(), 10, 10, 20, WHITE);
-        EndDrawing();
     }
 
     CloseWindow();
 }
 
+void ResetValues()
+{
+    while (!snake.empty())
+    {
+        snake.pop();
+    }
+
+    gameStarted = false;
+    direction = NONE;
+    score = 0;
+    frameCounter = 0;
+    food_x = 600;
+    food_y = 300;
+    gameState = PLAYING;
+}
